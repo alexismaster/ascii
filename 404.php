a11a7eba1e39ea253cdd3b56fa0e4a312635de30
Mathieu <gosselin.mathieug@gmail.com>
2014-11-22 17:10:38 -0500

    added htaccess to remove .php and added 404

---
+++ 404.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>ASCII</title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <link rel="shortcut icon" href="assets/img/compass.ico" type="image/x-icon">
+    <link rel="icon" href="assets/img/compass.ico" type="image/x-icon">
+
+    <link href='http://fonts.googleapis.com/css?family=Lora:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="assets/css/main.min.css">
+  </head>
+  <body>
+    <div id="main-container">
+      <main class="wrapper clearfix">
+        <h1 id="title">
+          <img src="assets/img/compass.png" alt="ASCII" title="ASCII">
+        </h1>
+        <h2 class="center">404 page</h2>
+        <p class="center">The page you are trying to acces doesn't exist.</p>
+      </main>
+    </div>
+  </body>
+</html>
